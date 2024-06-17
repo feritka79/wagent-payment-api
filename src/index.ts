1828843c3b1249b52b611a60df7a676c0fcfeb9a
ReihaneENS <ebrahiminsb@gmail.com>
2024-06-17 18:31:05 +0330

    added : packages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import connectDB from './config/config';
+import userRoutes from './routes/userRoutes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
 
-app.get('/a', (req, res) => {
-  res.send('???');
-});
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use(express.json());
+app.use('/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
