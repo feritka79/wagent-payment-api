114bb1fd7558d419df14d6734c45ad99fe1b84aa
ReihaneENS <ebrahiminsb@gmail.com>
2024-06-17 18:27:12 +0330

    added : auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+const jwt = require('jsonwebtoken');
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+    const token = (req.header('Authorization') as string)?.replace('Bearer ', '');
+
+    if (!token) {
+        return res.status(401).json({ error: { message: 'No token, authorization denied', code: 401 } });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as any;
+        req.user = decoded.id;  // Assign decoded id to req.user
+        next();
+    } catch (err) {
+        res.status(401).json({ error: { message: 'Token is not valid', code: 401 } });
+    }
+};
