a5f4150a4663528059b1b2e7ef6b4c2baebd7b55
Matin Kaboli <matinkaboli@aol.com>
2024-06-17 19:14:12 +0330

    fix: remove tsc and add log to connectDB

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try{
-        await mongoose.connect(process.env.DB_URI as string,{
+  try {
+    await mongoose.connect(process.env.DB_URI as string, {});
+  } catch (err) {
+    console.log("Error connecting to DB");
 
-        });
-    }catch (err){
-        process.exit(1);
-    }
-}
+    process.exit(1);
+  }
+};
 export default connectDb;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import connectDB from './config/config';
-import userRoutes from './routes/userRoutes';
-import dotenv from 'dotenv';
+import express from "express";
+import connectDB from "./config/config";
+import userRoutes from "./routes/userRoutes";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,7 +10,8 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
