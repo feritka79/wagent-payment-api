336d645c517ef806ce1814feab5cd2354e2225fe
Reyhane Ebrahiminasab <ebrahiminsb@gmail.com>
2024-06-20 11:12:43 +0330

    add : config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI as string, {});
+    // await mongoose.connect(process.env.DB_URI as string);
   } catch (err) {
     console.log("Error connecting to DB");
 
--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser, loginUser } from '../controllers/authController';
-import { validateUser } from '../middlewares/validationMiddleware';
-
-const router = express.Router();
-
-router.post('/auth',validateUser,registerUser);
-
-export default router;
