8c6c015e66b4f718dc7828f21696bf4501562805
ReihaneENS <ebrahiminsb@gmail.com>
2024-06-21 16:28:09 +0330

    fix : dbConnect and port

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import express from "express";
-import connectDB from "./config/config";
-import userRoutes from "./routes/userRoutes";
-import dotenv from "dotenv";
 
-dotenv.config();
+import getEnvs from "./env";
+import connectDb from "./config/connectDb";
+import userRoutes from "./routes/userRoutes";
+
 
 const app = express();
 
-connectDB();
+const { DB_URI, DB_NAME } = getEnvs();
+
+connectDb(DB_URI, DB_NAME)
+    .then(() => {
+        app.get('/a', (req, res) => {
+            res.send('Connected to database');
+        });
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Failed to connect to the database:', err);
+    });
 
 app.use(express.json());
 app.use("/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const {PORT} = getEnvs();
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
