c95963e64fea74a4b37e12f89f51b7e251c1fa39
Reyhane Ebrahiminasab <ebrahiminsb@gmail.com>
2024-06-29 16:18:55 +0330

    fix : connectDB

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,7 @@ const app = express();
 const {DB_URI, DB_NAME, PORT} = getEnvs();
 
 connectDb(DB_URI, DB_NAME)
-    .then(() => {
-        console.log('Connected to the database');
-    })
-    .catch(err => {
-        console.error('Failed to connect to the database:', err);
-    });
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
