50d4031f221b4521cd803799077f4acbf7a4d10f
ReihaneENS <ebrahiminsb@gmail.com>
2024-07-06 22:08:19 +0330

    fix : use multiple variables

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,15 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const token = (req.header('Authorization') as string)?.replace('Bearer ', '');
+    const authorization = (req.header('Authorization') as string);
+    const token = authorization.replace('Bearer ', '');
 
     if (!token) {
         return res.status(401).json({ error: { message: 'No token, authorization denied', code: 401 } });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as any;
-        req.user = decoded.id;  // Assign decoded id to req.user
+        const secret = process.env.JWT_SECRET as string;
+        const decodedToken = jwt.verify(token, secret);
+        const decoded = decodedToken as any;
+        req.user = decoded.id;
         next();
     } catch (err) {
         res.status(401).json({ error: { message: 'Token is not valid', code: 401 } });
