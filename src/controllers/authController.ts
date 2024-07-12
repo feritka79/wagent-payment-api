ad51c78f65d65dee6d39480cbb50c5cc907bd45e
ReihaneENS <ebrahiminsb@gmail.com>
2024-07-12 23:54:20 +0330

    feat : add authController

---
+++ src/controllers/authController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from 'express';
+import { User } from '../models/User';
+import { sendRegistrationEmail } from '../utils/emailService';
+import { validateUser } from '../middlewares/validationMiddleware';
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id: string) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+};
+
+export const registerUser = [
+    validateUser,
+    async (req: Request, res: Response) => {
+        const { storeName, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ message: 'User Already Exist!' });
+            }
+
+            user = new User({ storeName, email, password });
+            await user.save();
+
+            sendRegistrationEmail(email);
+
+            const token = generateToken(user.id);
+
+            res.json({
+                result: {
+                    id: user.id,
+                    email: user.email,
+                    storeName: user.storeName,
+                    storeImage: user.storeImage,
+                    token,
+                },
+            });
+        } catch (err) {
+            res.status(500).json({ error: { message: 'Error Occurred!', code: 500 } });
+        }
+    },
+];
+
+export const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user || !(await user.matchPassword(password))) {
+            return res.status(400).json({ error: { message: 'Invalid credentials', code: 400 } });
+        }
+        const token = generateToken(user.id);
+
+        res.json({
+            result: {
+                id: user.id,
+                email: user.email,
+                storeName: user.storeName,
+                token,
+            },
+        });
+    } catch (err) {
+        res.status(500).json({ error: { message: 'Error Occurred!', code: 500 } });
+    }
+};
