a46684e056dbd722f761b80a34023b2b038853d5
ReihaneENS <ebrahiminsb@gmail.com>
2024-07-13 19:37:31 +0330

    fix : separate import from files

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+
 import { User } from '../models/User';
 import { sendRegistrationEmail } from '../utils/emailService';
 import { validateUser } from '../middlewares/validationMiddleware';
